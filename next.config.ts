02236e19461a5836ef98fddf8d72a0485274c07e
Thiago <thiago.lanzarin2812@gmail.com>
2026-05-31 20:47:52 -0300

    Add experimental configuration for allowed development origins

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    allowedDevOrigins: ['192.168.137.1', '192.168.137.1:3000'],
+  },
 };
 
 export default nextConfig;
